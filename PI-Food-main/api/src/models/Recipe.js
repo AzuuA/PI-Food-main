37c3ce6af69e00124ad075efba23d68e26bf0e67
Azul <azul235arangui@gmail.com>
2023-03-14 11:55:38 -0300

    Modelos v1

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -1,12 +1,46 @@
-const { DataTypes } = require('sequelize');
+const {  DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
     },
-  });
+
+    name: {
+      type: DataTypes.STRING(80),
+      allowNull: false, // require *
+      validate: {        
+        len: [6, 150]
+      } 
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://i.postimg.cc/t4F6nM5b/324.png",
+      validate: {isUrl: true}
+    },
+
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+
+    healthScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {min: 0, max: 100}
+    },
+
+    steps: {
+      type: DataTypes.ARRAY(DataTypes.TEXT)
+    }
+  },
+  { timestamps: false }
+  );
 };
--- PI-Food-main/api/src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getUserHandler,postUserHandler,getUserIdHandler} = require("../handlers/usersHandler")
-const routerUsers = Router();
-
-routerUsers.get("/",getUserHandler);
-routerUsers.get("/:idUser",getUserIdHandler);
-routerUsers.post("/create",postUserHandler);
-
-module.exports=routerUsers
