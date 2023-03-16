cb2447ee277fb3bd37cf695496cbbe1ac2d76274
Azul <azul235arangui@gmail.com>
2023-03-16 20:32:31 -0300

    Routas del index

--- PI-Food-main/api/src/routes/index.js
+++ PI-Food-main/api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const routerRecipes = require('./recipes');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+
+const routerDiets = require('./diets');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+ router.use("/recipes",routerRecipes);
+ router.use("/diets",routerDiets);
 
 module.exports = router;
