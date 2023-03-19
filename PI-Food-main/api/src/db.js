7ef70b0f687a502c4b9cdcebb6c0b8b7fca10926
Azul <azul235arangui@gmail.com>
2023-03-19 09:49:05 -0300

    DB

--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -30,9 +30,9 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe,Diet} = sequelize.models;
-Recipe.belongsToMany(Diet, { through: 'recipe_diet' });
-Diet.belongsToMany(Recipe, { through: 'recipe_diet' });
+const { Recipe , TypeDiet } = sequelize.models;
+Recipe.belongsToMany(TypeDiet, {through: 'recipeTypediet'})
+TypeDiet.belongsToMany(Recipe, {through: 'recipeTypediet'})
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
